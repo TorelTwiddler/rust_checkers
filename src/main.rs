319831417dcee42b0fffc952e9afe49a4551a558
Salvatore Lopiparo <toreltwiddler@gmail.com>
2015-05-31 09:05:22 -0700

    added the new_game method

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
-
-const BOARD_SIZE : usize = 8;
-
 fn main() {
-  let mut board: Board = Default::default();
-  board.print();
-  board.places[2][3] = Some(Piece {id: 4});
-  board.print();
+    let mut board: Board = Default::default();
+    board.print();
+    board.new_game();
+    board.print();
 }
 
 struct Board {
-    places: [[Option<Piece>; BOARD_SIZE]; BOARD_SIZE],
+    places: [[Option<Piece>; 8]; 8],
 }
 
-impl Board { fn print(&self) {
+impl Board {
+    fn print(&self) {
         println!("---- Board ----");
-        for i in 1 .. BOARD_SIZE {
-            for j in 1 .. BOARD_SIZE {
+        for i in 0 .. 8 {
+            for j in 0 .. 8 {
                 match self.places[i][j] {
-                    Some(Piece {id, .. }) => print!("{:?}", id),
+                    Some(Piece {player, .. }) => print!("{:?}", player),
                     None => print!("0"),
                 }
             }
             println!(" ");
+        }
+    }
+
+    fn new_game(&mut self) {
+        for j in 0 .. 8 {
+            for i in 0 .. 3 {
+                if (i + j) % 2 == 0 {
+                    self.places[i][j] = Some(Piece {player: 2});
+                }
+            }
+            for i in 5 .. 8 {
+                if (i + j) % 2 == 0 {
+                    self.places[i][j] = Some(Piece {player: 1});
+                }
+            }
         }
     }
 }
@@ -29,12 +42,12 @@ impl Board { fn print(&self) {
 impl Default for Board {
     fn default() -> Board {
         Board {
-            places: [[None; BOARD_SIZE]; BOARD_SIZE],
+            places: [[None; 8]; 8],
         }
     }
 }
 
 #[derive(Debug, Copy, Clone)]
 struct Piece {
-    id: i32,
+    player: i32,
 }
