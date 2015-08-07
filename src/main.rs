0f560a330cbd1ff963e76662cfa0fd483577b11c
Salvatore Lopiparo <toreltwiddler@gmail.com>
2015-08-06 21:07:27 -0700

    can now move pieces!

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     board.print();
     board.new_game();
     board.print();
+    println!("\n{:?}", board.move_piece(Position {x: 0, y: 0}, Position {x: 0, y: 1}).is_ok());
+    board.print();
 }
 
 struct Board {
@@ -37,6 +39,21 @@ impl Board {
             }
         }
     }
+
+    fn move_piece(&mut self, from: Position, to: Position) -> Result<(), &str> {
+        let piece: Option<Piece> = self.get_at_position(from);
+        if piece.is_some() {
+            self.places[to.x][to.y] = piece; 
+            self.places[from.x][from.y] = None;
+            return Ok(());
+        } else {
+            return Err("Invalid move");
+        }
+    }
+
+    fn get_at_position(&self, position: Position) -> Option<Piece> {
+        self.places[position.x][position.y]
+    }
 }
 
 impl Default for Board {
@@ -50,4 +67,10 @@ impl Default for Board {
 #[derive(Debug, Copy, Clone)]
 struct Piece {
     player: i32,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Position {
+    x: usize,
+    y: usize,
 }
